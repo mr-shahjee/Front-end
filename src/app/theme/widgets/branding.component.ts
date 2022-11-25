c9a6c2ecbd95eb92607a5aefc83310b17493ad26
mr-shahjee <abdullahshahjee7@gmail.com>
2022-11-25 17:43:58 +0500

    new repo commit

---
+++ src/app/theme/widgets/branding.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-branding',
+  template: `
+    <a class="matero-branding" href="/">
+      <span class="matero-branding-name" style="color: white;">Hospital Management</span>
+    </a>
+  `,
+})
+export class BrandingComponent {}
